6dd3791c25e313f8512372c2781ce55123450992
rob <R.V@post.cz>
2020-03-29 02:17:10 +0100

    feat: add userDetail data generator

--- RobinEshop/RobinEshop/Models/EshopDbContext.cs
+++ RobinEshop/RobinEshop/Models/EshopDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Collections.Generic;
+using Bogus;
+using Microsoft.EntityFrameworkCore;
 
 namespace RobinEshop.Models
 {
@@ -41,9 +43,27 @@ namespace RobinEshop.Models
             modelBuilder.Entity<Category>().HasMany(category => category.Products ).WithOne(product => product.Category);
             // one-to-one
             modelBuilder.Entity<User>().HasOne(user => user.UserDetail).WithOne(userDetail => userDetail.User);
-            
-            
-            
+            this.TestData(modelBuilder);
+        }
+
+        private void TestData(ModelBuilder modelBuilder)
+        {
+            List<UserDetail> userDetails = new List<UserDetail>();
+            for (int i = 0; i < 5; i++)
+            {
+                userDetails.Add(
+                new Faker<UserDetail>().RuleFor(u => u.UserDetailId, (f, u) => i+1)
+                 .RuleFor(u => u.FirstName, (f, u) => f.Name.FirstName())
+                .RuleFor(u => u.LastName, (f, u) => f.Name.LastName())
+                .RuleFor(u => u.ContactEmail, (f, u) => f.Internet.Email(u.FirstName, u.LastName))
+                .RuleFor(u => u.Address, (f, u) => f.Address.StreetAddress())
+                .RuleFor(u => u.City, (f, u) => f.Address.City())
+                .RuleFor(u => u.Country, (f, u) => f.Address.Country())
+                .RuleFor(u => u.ZipCode, (f, u) => f.Address.ZipCode())
+                );
+            }
+
+            modelBuilder.Entity<UserDetail>().HasData(userDetails);
         }
     }
 }
